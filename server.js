ca4124195e7fce49835352a1e0366e166df9bcea
Krazytop <78879663+krazytop@users.noreply.github.com>
2024-06-12 20:47:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 443;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, '/dist/krazytop-front')));
 
