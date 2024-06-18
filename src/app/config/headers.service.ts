a6966958bf72d95bdf00bff1b18328aed4bda055
clement_tell <clement.tell44@gmail.com>
2024-06-18 17:06:22 +0200

    try to update routes

--- src/app/config/headers.service.ts
+++ src/app/config/headers.service.ts
@@ -4,29 +4,12 @@ import {Injectable} from '@angular/core';
   providedIn: 'root',
 })
 export class HeaderService {
-  private static instance: HeaderService;
 
-  private headers: { [key: string]: string } = {};
-
-  constructor() {
-    console.log("init HeaderService")
-    if (!HeaderService.instance) {
-      HeaderService.instance = this;
-    }
-
-    const username = 'krazytop';
-    const password = 'password';
-    const base64Credentials = btoa(username + ':' + password);
-    this.headers['Authorization'] = 'Basic ' + base64Credentials;
-
-    return HeaderService.instance;
-  }
+  private static headers: { [key: string]: string } = {
+    'Authorization' : 'Basic ' + btoa('krazytop' + ':' + 'password')
+  };
 
   static getHeaders(): { [key: string]: string } {
-    if (HeaderService.instance) {
-      return HeaderService.instance.headers;
-    } else {
-      throw new Error('HeaderService not initialized.');
-    }
+    return HeaderService.headers;
   }
 }
