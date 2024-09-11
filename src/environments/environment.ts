6bc74192f7449b43c9df357adf936e2be5f1fff7
clement_tell <clement.tell44@gmail.com>
2024-09-11 14:41:46 +0200

    .com => .fr

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://api.krazytop.com/',
+  apiURL: 'https://api.krazytop.fr/',
   apiKeyBungie: '043641212e7e460ca38737cbe0e93203',
   apiClientIdBungie: '46584'
 };
