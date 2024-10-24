e38948e20550df45243502627339eaf4f0101dae
Krazytop <clement.tell44@gmail.com>
2024-10-24 23:03:04 +0200

    to desktop

--- src/app/model/destiny/destiny-presentation-trees.model.ts
+++ src/app/model/destiny/destiny-presentation-trees.model.ts
@@ -1,11 +1,11 @@
 import {DestinyPresentationTreeNomenclature} from "./destiny-presentation-tree.model";
 
 export class DestinyPresentationTreesModel {
-  titles?: DestinyPresentationTreeNomenclature;
-  archivedTitles?: DestinyPresentationTreeNomenclature;
-  catalysts?: DestinyPresentationTreeNomenclature;
-  kineticWeaponModels?: DestinyPresentationTreeNomenclature;
-  energyWeaponModels?: DestinyPresentationTreeNomenclature;
-  powerWeaponModels?: DestinyPresentationTreeNomenclature;
-  badges?: DestinyPresentationTreeNomenclature;
+  titles!: DestinyPresentationTreeNomenclature;
+  archivedTitles!: DestinyPresentationTreeNomenclature;
+  catalysts!: DestinyPresentationTreeNomenclature;
+  kineticWeaponModels!: DestinyPresentationTreeNomenclature;
+  energyWeaponModels!: DestinyPresentationTreeNomenclature;
+  powerWeaponModels!: DestinyPresentationTreeNomenclature;
+  badges!: DestinyPresentationTreeNomenclature;
 }
