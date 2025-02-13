7f625f1cbd39bfb4af135f211e5e56d2338f4ba3
clement-tell <candidat.clement.tell44@gmail.com>
2025-02-13 11:15:58 +0100

    express 5 fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4200;
 
 app.use(express.static(path.join(__dirname, '/dist/krazytop-front')));
 
-app.get('*', (req, res) => {
+app.get('{/*path}', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/krazytop-front/index.html'));
 })
 
