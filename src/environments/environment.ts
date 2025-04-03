752edf1595c4f824c721b8eaf532d3f396743fd4
krazytop <clement.tell44@gmail.com>
2025-04-03 18:02:18 +0200

    fr => com

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://api.krazytop.fr/',
+  apiURL: 'https://api.krazytop.com/',
   apiKeyBungie: '043641212e7e460ca38737cbe0e93203',
   apiClientIdBungie: '46584',
   updateRIOTFrequency: 60,
